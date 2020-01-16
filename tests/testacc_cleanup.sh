32fa4f275123499ee6775589531e3cab9ff566e9
Grzegorz Abram <grzegorz.abram@snapshot.travel>
2020-01-16 13:50:15 +0100

    CLEANUP

--- tests/testacc_cleanup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source "$(pwd)"/tests/env.sh
-docker-compose -f "$(pwd)"/tests/docker-compose.yml down
-unset TF_ACC PGHOST PGPORT PGUSER PGPASSWORD PGSSLMODE
--- tests/testacc_setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source "$(pwd)"/tests/env.sh
-docker-compose -f "$(pwd)"/tests/docker-compose.yml up -d
-"$(pwd)"/tests/wait-postgres-docker.sh "$(pwd)"/tests/docker-compose.yml
